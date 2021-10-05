50d5dfcfc34859fbf5d38301254c1ebfb451758e
Igor <igormgg@hotmail.com>
2021-10-05 16:01:06 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./reset.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Criando tema dark / light</header>
+    </div>
+  );
+}
+
+export default App;
