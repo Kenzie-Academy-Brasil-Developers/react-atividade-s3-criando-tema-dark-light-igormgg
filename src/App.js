58793a811dcb7f86fffd00dc2936ecbd2c8fc515
Igor <igormgg@hotmail.com>
2021-10-05 20:38:08 -0300

    feat: work done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import "./reset.css";
-import "./App.css";
+import { useCallback, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { AppContainer, MainSection } from "./components/app.style";
+import { themes } from "./themes";
+import { Button } from "./components/button.style";
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const getOppositeTheme = useCallback(
+    () => (currentTheme === "light" ? "dark" : "light"),
+    [currentTheme]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">Criando tema dark / light</header>
-    </div>
+    <ThemeProvider theme={themes[currentTheme]}>
+      <AppContainer>
+        <MainSection>
+          <h1>You are in {currentTheme} mode</h1>
+          <Button onClick={() => setCurrentTheme(getOppositeTheme())}>
+            switch to {getOppositeTheme()} mode
+          </Button>
+        </MainSection>
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
